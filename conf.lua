967d72dc7cf9e5dbc62c9a76ce2dd4f366de43a6
CWRules <36826436+CWRules@users.noreply.github.com>
2018-02-25 19:22:39 -0500

    Function comments
    Added description of what the conf() function does.
    Added more application configuration settings.

--- conf.lua
+++ conf.lua
@@ -1,3 +1,10 @@
+
+--[[ love.conf()
+  Overrides default LOVE configuration function.
+--]]
 function love.conf(t)
-    t.window.title = "Car Physics Test"
+  t.window.title = "Car Physics Test"
+  t.window.width = 1000
+  t.window.height = 600
+  t.modules.joystick = false
 end
